2b6a26ba77a14d27a99fd879750793c520235573
haramnasir24 <haramn518@gmail.com>
2026-01-30 02:01:08 +0500

    hero section: increased breakpoint to 768px

--- src/components/home/hero-section.tsx
+++ src/components/home/hero-section.tsx
@@ -25,17 +25,17 @@ export function HeroSection() {
       }}
     >
       <div className="max-w-[1440px] mx-auto">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-10 xl:gap-12 2xl:gap-16 items-center min-h-[calc(100vh-160px)]">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-8 lg:gap-10 xl:gap-12 2xl:gap-16 items-center min-h-[calc(100vh-160px)]">
           {/* Left Content */}
           <div className="relative flex flex-col justify-center space-y-6 lg:space-y-6 xl:space-y-7 2xl:space-y-8">
-            {/* Decorative Border */}
-            <div className="hidden lg:block absolute lg:-top-3 xl:-top-4 2xl:-top-5 lg:right-10 xl:right-14 2xl:right-20 lg:w-48 xl:w-52 2xl:w-60 h-50 border-t border-r border-r-gray-950 border-t-gray-950 opacity-100"></div>
+            {/* Decorative Border - medium and up (positioning: md, then lg/xl/2xl) */}
+            <div className="hidden md:block absolute md:-top-3 md:right-6 md:w-40 lg:-top-3 xl:-top-4 2xl:-top-5 lg:right-10 xl:right-14 2xl:right-20 lg:w-48 xl:w-52 2xl:w-60 h-50 border-t border-r border-r-gray-950 border-t-gray-950 opacity-100"></div>
 
             {/* Decorative Border mobile */}
-            <div className="lg:hidden absolute -top-4 left-1 w-30 h-30 border-t border-l border-l-gray-950 border-t-gray-950 opacity-100"></div>
+            <div className="md:hidden absolute -top-4 left-1 w-30 h-30 border-t border-l border-l-gray-950 border-t-gray-950 opacity-100"></div>
 
             <p
-              className="text-lg tracking-wide font-satoshi lg:text-left text-center"
+              className="text-lg tracking-wide font-satoshi md:text-left text-center"
               style={{
                 color: "#1A1A1A",
                 fontWeight: 400,
@@ -45,7 +45,7 @@ export function HeroSection() {
             </p>
 
             <h1
-              className={`lg:hidden text-4xl sm:text-5xl text-center leading-tight ${tenorSans.className}`}
+              className={`md:hidden text-4xl sm:text-5xl text-center leading-tight ${tenorSans.className}`}
               style={{
                 fontWeight: 400,
                 color: "#1A1A1A",
@@ -55,7 +55,7 @@ export function HeroSection() {
             </h1>
 
             <h1
-              className={`hidden lg:block text-5xl sm:text-6xl lg:text-7xl xl:text-8xl leading-[1.1] ${tenorSans.className}`}
+              className={`hidden md:block text-5xl sm:text-6xl lg:text-7xl xl:text-8xl leading-[1.1] ${tenorSans.className}`}
               style={{
                 fontWeight: 400,
                 color: "#000000",
@@ -68,8 +68,8 @@ export function HeroSection() {
               Guide
             </h1>
 
-            {/* Mobile Decorative Elements */}
-            <div className="relative h-[320px] lg:hidden my-8 mx-auto max-w-[400px] w-full">
+            {/* Mobile Decorative Elements (center - only on small screens) */}
+            <div className="relative h-[320px] md:hidden my-8 mx-auto max-w-[400px] w-full">
               {/* Gradient Ellipse Background */}
               <div
                 className={`w-full h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 scale-140 ${pulseSlowAnimation}`}
@@ -210,7 +210,7 @@ export function HeroSection() {
             </div>
 
             <p
-              className="text-base lg:text-lg leading-relaxed lg:max-w-md xl:max-w-[480px] 2xl:max-w-lg font-satoshi text-center lg:text-left"
+              className="text-base lg:text-lg leading-relaxed md:max-w-sm lg:max-w-md xl:max-w-[480px] 2xl:max-w-lg font-satoshi text-center md:text-left"
               style={{
                 color: "#1A1A1A",
                 fontWeight: 300,
@@ -296,11 +296,11 @@ export function HeroSection() {
             </div>
           </div>
 
-          {/* Right Decorative Elements */}
-          <div className="relative lg:h-[550px] xl:h-[600px] 2xl:h-[700px] hidden lg:block">
+          {/* Right Decorative Elements (same layout from md as large screens) */}
+          <div className="relative md:h-[450px] lg:h-[550px] xl:h-[600px] 2xl:h-[700px] hidden md:block">
             {/* Gradient Ellipse Background */}
             <div
-              className={`w-full h-full absolute lg:top-[48%] lg:left-[48%] xl:top-[48%] xl:left-[48%] 2xl:top-1/2 2xl:left-1/2 -translate-x-1/2 -translate-y-1/2 lg:scale-100 xl:scale-105 2xl:scale-110 ${pulseSlowAnimation}`}
+              className={`w-full h-full absolute md:top-[48%] md:left-[48%] lg:top-[48%] lg:left-[48%] xl:top-[48%] xl:left-[48%] 2xl:top-1/2 2xl:left-1/2 -translate-x-1/2 -translate-y-1/2 md:scale-95 lg:scale-100 xl:scale-105 2xl:scale-110 ${pulseSlowAnimation}`}
             >
               <Image
                 src="/assets/gradient-ellipse.svg"
@@ -312,12 +312,12 @@ export function HeroSection() {
 
             {/* Playbook */}
             <div
-              className={`absolute lg:top-[23%] lg:left-[20%] xl:top-[24%] xl:left-[20%] z-10 ${floatAnimation}`}
+              className={`absolute md:top-[23%] md:left-[20%] lg:top-[23%] lg:left-[20%] xl:top-[24%] xl:left-[20%] z-10 ${floatAnimation}`}
             >
               <Image
                 src="/assets/playbook.svg"
                 alt="Playbook"
-                className="lg:w-[90px] lg:h-[90px] xl:w-[120px] xl:h-[120px] drop-shadow-lg"
+                className="md:w-[70px] md:h-[70px] lg:w-[90px] lg:h-[90px] xl:w-[120px] xl:h-[120px] drop-shadow-lg"
                 width={120}
                 height={120}
               />
@@ -325,24 +325,24 @@ export function HeroSection() {
 
             {/* Top Right Circle with Flower */}
             <div
-              className={`absolute lg:top-[15%] lg:right-[20%] xl:top-[18%] xl:right-[28%] z-10 ${pulseSlowAnimation}`}
+              className={`absolute md:top-[15%] md:right-[20%] lg:top-[15%] lg:right-[20%] xl:top-[18%] xl:right-[28%] z-10 ${pulseSlowAnimation}`}
             >
               <Image
                 src="/assets/ellipse1_2.svg"
                 alt=""
-                className="lg:w-[150px] lg:h-[160px] xl:w-[160px] xl:h-[180px] opacity-100"
+                className="md:w-[120px] md:h-[130px] lg:w-[150px] lg:h-[160px] xl:w-[160px] xl:h-[180px] opacity-100"
                 width={200}
                 height={180}
               />
             </div>
             <div
-              className={`absolute lg:top-[15%] lg:right-[20%] xl:top-[18%] xl:right-[28%] z-20 ${bobAnimation}`}
+              className={`absolute md:top-[15%] md:right-[20%] lg:top-[15%] lg:right-[20%] xl:top-[18%] xl:right-[28%] z-20 ${bobAnimation}`}
             >
-              <div className="lg:w-[160px] lg:h-[160px] xl:w-[180px] xl:h-[180px] flex items-center justify-center">
+              <div className="md:w-[130px] md:h-[130px] lg:w-[160px] lg:h-[160px] xl:w-[180px] xl:h-[180px] flex items-center justify-center">
                 <Image
                   src="/assets/flower1.svg"
                   alt=""
-                  className="lg:w-[95px] lg:h-[95px] xl:w-[100px] xl:h-[120px]"
+                  className="md:w-[70px] md:h-[80px] lg:w-[95px] lg:h-[95px] xl:w-[100px] xl:h-[120px]"
                   width={120}
                   height={120}
                 />
@@ -351,36 +351,36 @@ export function HeroSection() {
 
             {/* Cactus */}
             <div
-              className={`absolute lg:top-[32%] lg:right-[8%] xl:top-[35%] xl:right-[20%] z-20 ${floatDelayAnimation}`}
+              className={`absolute md:top-[32%] md:right-[8%] lg:top-[32%] lg:right-[8%] xl:top-[35%] xl:right-[20%] z-20 ${floatDelayAnimation}`}
             >
-              <Image 
-                src="/assets/cactus.svg" 
-                alt="" 
-                className="lg:w-[40px] lg:h-[40px] xl:w-[50px] xl:h-[50px]"
-                width={50} 
-                height={50} 
+              <Image
+                src="/assets/cactus.svg"
+                alt=""
+                className="md:w-[35px] md:h-[35px] lg:w-[40px] lg:h-[40px] xl:w-[50px] xl:h-[50px]"
+                width={50}
+                height={50}
               />
             </div>
 
             {/* Middle Right Circle with Flower (Fern) */}
             <div
-              className={`absolute lg:top-[50%] lg:right-[8%] xl:top-[50%] xl:right-[16%] 2xl:top-[50%] 2xl:right-[20%] z-10 ${pulseSlowAnimation}`}
+              className={`absolute md:top-[50%] md:right-[8%] lg:top-[50%] lg:right-[8%] xl:top-[50%] xl:right-[16%] 2xl:top-[50%] 2xl:right-[20%] z-10 ${pulseSlowAnimation}`}
             >
               <Image
                 src="/assets/ellipse3.svg"
                 alt=""
-                className="lg:w-[150px] lg:h-[150px] xl:w-[165px] xl:h-[165px] 2xl:w-[180px] 2xl:h-[180px] opacity-100"
+                className="md:w-[120px] md:h-[120px] lg:w-[150px] lg:h-[150px] xl:w-[165px] xl:h-[165px] 2xl:w-[180px] 2xl:h-[180px] opacity-100"
                 width={180}
                 height={180}
               />
             </div>
             <div
-              className={`absolute lg:top-[52%] lg:right-[14%] xl:top-[52%] xl:right-[21%] 2xl:top-[53%] 2xl:right-[25%] z-10 ${bobDelayAnimation}`}
+              className={`absolute md:top-[52%] md:right-[14%] lg:top-[52%] lg:right-[14%] xl:top-[52%] xl:right-[21%] 2xl:top-[53%] 2xl:right-[25%] z-10 ${bobDelayAnimation}`}
             >
               <Image
                 src="/assets/flower3.svg"
                 alt=""
-                className="lg:w-[80px] lg:h-[120px] xl:w-[90px] xl:h-[135px] 2xl:w-[100px] 2xl:h-[150px]"
+                className="md:w-[60px] md:h-[90px] lg:w-[80px] lg:h-[120px] xl:w-[90px] xl:h-[135px] 2xl:w-[100px] 2xl:h-[150px]"
                 width={100}
                 height={150}
               />
@@ -388,24 +388,24 @@ export function HeroSection() {
 
             {/* Bottom Left Circle with Flower */}
             <div
-              className={`absolute lg:bottom-[22%] lg:left-[16%] xl:bottom-[22%] xl:left-[18%] 2xl:bottom-[20%] 2xl:left-[15%] z-10 ${pulseSlowAnimation}`}
+              className={`absolute md:bottom-[22%] md:left-[16%] lg:bottom-[22%] lg:left-[16%] xl:bottom-[22%] xl:left-[18%] 2xl:bottom-[20%] 2xl:left-[15%] z-10 ${pulseSlowAnimation}`}
             >
               <Image
                 src="/assets/ellipse1_2.svg"
                 alt=""
-                className="lg:w-[160px] lg:h-[160px] xl:w-[175px] xl:h-[170px] 2xl:w-[200px] 2xl:h-[180px] opacity-100"
+                className="md:w-[130px] md:h-[130px] lg:w-[160px] lg:h-[160px] xl:w-[175px] xl:h-[170px] 2xl:w-[200px] 2xl:h-[180px] opacity-100"
                 width={200}
                 height={180}
               />
             </div>
 
             <div
-              className={`absolute lg:bottom-[25%] lg:left-[20%] xl:bottom-[25%] xl:left-[22%] 2xl:bottom-[25%] 2xl:left-[20%] z-10 ${floatAnimation}`}
+              className={`absolute md:bottom-[25%] md:left-[20%] lg:bottom-[25%] lg:left-[20%] xl:bottom-[25%] xl:left-[22%] 2xl:bottom-[25%] 2xl:left-[20%] z-10 ${floatAnimation}`}
             >
               <Image
                 src="/assets/flower2.svg"
                 alt=""
-                className="lg:w-[105px] lg:h-[105px] xl:w-[115px] xl:h-[115px] 2xl:w-[130px] 2xl:h-[130px]"
+                className="md:w-[85px] md:h-[85px] lg:w-[105px] lg:h-[105px] xl:w-[115px] xl:h-[115px] 2xl:w-[130px] 2xl:h-[130px]"
                 width={130}
                 height={130}
               />
@@ -413,12 +413,12 @@ export function HeroSection() {
 
             {/* Cat Paw */}
             <div
-              className={`absolute lg:bottom-[12%] lg:right-[34%] xl:bottom-[12%] xl:right-[38%] 2xl:bottom-[10%] 2xl:right-[40%] z-20 ${bobAnimation}`}
+              className={`absolute md:bottom-[12%] md:right-[34%] lg:bottom-[12%] lg:right-[34%] xl:bottom-[12%] xl:right-[38%] 2xl:bottom-[10%] 2xl:right-[40%] z-20 ${bobAnimation}`}
             >
               <Image
                 src="/assets/cat-paw.svg"
                 alt=""
-                className="lg:w-[85px] lg:h-[85px] xl:w-[95px] xl:h-[95px] 2xl:w-[110px] 2xl:h-[110px] drop-shadow-lg"
+                className="md:w-[70px] md:h-[70px] lg:w-[85px] lg:h-[85px] xl:w-[95px] xl:h-[95px] 2xl:w-[110px] 2xl:h-[110px] drop-shadow-lg"
                 width={110}
                 height={110}
               />
@@ -426,25 +426,25 @@ export function HeroSection() {
 
             {/* Stars */}
             <div
-              className={`absolute lg:top-[18%] lg:left-[18%] xl:top-[18%] xl:left-[16%] 2xl:top-[15%] 2xl:left-[15%] z-5 ${rotateSlowAnimation}`}
+              className={`absolute md:top-[18%] md:left-[18%] lg:top-[18%] lg:left-[18%] xl:top-[18%] xl:left-[16%] 2xl:top-[15%] 2xl:left-[15%] z-5 ${rotateSlowAnimation}`}
             >
               <Image
                 src="/assets/hero-star.svg"
                 alt=""
-                className="lg:w-[24px] lg:h-[24px] xl:w-[27px] xl:h-[27px] 2xl:w-[30px] 2xl:h-[30px]"
+                className="md:w-[20px] md:h-[20px] lg:w-[24px] lg:h-[24px] xl:w-[27px] xl:h-[27px] 2xl:w-[30px] 2xl:h-[30px]"
                 width={30}
                 height={30}
               />
             </div>
             <div
-              className={`absolute lg:top-[48%] lg:right-[10%] xl:top-[48%] xl:right-[16%] 2xl:top-[48%] 2xl:right-[20%] z-30 ${rotateSlowAnimation}`}
+              className={`absolute md:top-[48%] md:right-[10%] lg:top-[48%] lg:right-[10%] xl:top-[48%] xl:right-[16%] 2xl:top-[48%] 2xl:right-[20%] z-30 ${rotateSlowAnimation}`}
             >
-              <Image 
-                src="/assets/star2.svg" 
-                alt="" 
-                className="lg:w-[28px] lg:h-[28px] xl:w-[31px] xl:h-[31px] 2xl:w-[35px] 2xl:h-[35px]"
-                width={30} 
-                height={30} 
+              <Image
+                src="/assets/star2.svg"
+                alt=""
+                className="md:w-[24px] md:h-[24px] lg:w-[28px] lg:h-[28px] xl:w-[31px] xl:h-[31px] 2xl:w-[35px] 2xl:h-[35px]"
+                width={30}
+                height={30}
               />
             </div>
           </div>
